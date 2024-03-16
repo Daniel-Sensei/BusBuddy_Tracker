432e67c760098e548cbc885d8181494fb0947198
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-03-16 14:06:34 +0100

    Added GPS Permissions

--- src/app/track-button/track-button.component.ts
+++ src/app/track-button/track-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation, PositionOptions } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-track-button',
@@ -7,8 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackButtonComponent  implements OnInit {
 
+  currentPosition: any = undefined;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getCurrentPosition();
+  }
+
+  async getCurrentPosition() {
+    console.log("Getting current position");
+    try {
+      const permissionStatus = await Geolocation.checkPermissions();
+      console.log("Permission status: ", permissionStatus.location);
+
+      if (permissionStatus.location !== 'granted') {
+        const requestStatus = await Geolocation.requestPermissions();
+        console.log("Permission request: ", requestStatus.location);
+        if (requestStatus.location !== 'granted') {
+          throw new Error('Permission not granted');
+        }
+      }
+
+      let options: PositionOptions = {
+        maximumAge: 3000,
+        timeout: 10000,
+        enableHighAccuracy: true
+      };
+      this.currentPosition = await Geolocation.getCurrentPosition(options);
+    } catch (error) {
+      console.error('Error getting current position', error);
+    }
+  }
 
 }
