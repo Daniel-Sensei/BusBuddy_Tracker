65f507f555f0cee24d980f4a4e54daecf3d329ed
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-03-29 20:31:06 +0100

    Deleted old stuff after changed track-button-component into a page

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-import { TrackButtonComponentModule } from '../track-button/track-button.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    TrackButtonComponentModule,
     Tab1PageRoutingModule
   ],
   declarations: [Tab1Page]
